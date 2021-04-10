70b12021987e0ab20831fa731321a82fb5a2b1dc
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-04-10 18:40:17 +0100

    alpha_vantage retrieve for multiple_tickers.

--- 1_getting_data/alpha_vantage_script.py
+++ 1_getting_data/alpha_vantage_script.py
@@ -1,4 +1,21 @@
-from alpha_vantage.timeseries import TimeSeries
+import time
 
-ts = TimeSeries(key='API_KEY')
-data, meta_data = ts.get_intraday('GOOGL')
+from alpha_vantage.timeseries import TimeSeries
+import pandas as pd
+
+key_path = 'D:\\Projects\\pcdv-algo-trading\\1_getting_data\\alpha_vantage_key.txt'
+
+# extracting data for a single ticker
+ts = TimeSeries(key=open(key_path,'r').read(), output_format='pandas')
+
+#data = ts.get_daily(symbol='EURUSD', outputsize='full')[0]
+#data.columns = ["open", "high", "low", "close", "volume"]
+
+all_tickers = ["AAPL", "MSFT", "CSCO", "AMZN", "GOOG"]
+close_prices = pd.DataFrame()
+for ticker in all_tickers:
+    start_time = time.time()
+    data = ts.get_intraday(symbol=ticker, interval='1min', outputsize='full')[0]
+    data.columns = ["open", "high", "low", "close", "volume"]
+    close_prices[ticker] = data["close"]
+
