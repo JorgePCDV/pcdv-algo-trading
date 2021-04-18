9c3a58aa5f5e79c05f9a8322600b8b989d5256c3
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2021-04-18 16:36:25 +0100

    handling_nan init.

---
+++ 2_working_with_data/handling_nan.py
@@ -0,0 +1,12 @@
+import datetime as dt
+import yfinance as yf
+import pandas as pd
+
+stocks = ['AMZN','MSFT','FB','GOOG']
+start = dt.datetime.today()-dt.timedelta(3650)
+end = dt.datetime.today()
+cl_price = pd.DataFrame()
+ohlcv_data = {}
+
+for ticker in stocks:
+    cl_price[ticker] = yf.download(ticker,start,end)['Adj Close']
